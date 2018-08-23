fce237b4055bf5ccac2b46efc723ea9b65956b1b
Pravin Angolkar <pravinkumar.angolkar@analog.com>
2018-08-23 16:38:54 +0530

    Binary Query

--- binaryQueries.cpp
+++ binaryQueries.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 
-void printArrayInReverseOrder(int arraySize, int arr[]){
-	for(int i = arraySize - 1; i >= 0; i--){
-		std::cout << arr[i] << std::endl;
-	}
-}
-
 int main(int argc, char const *argv[])
 {
-	int arraySize;
-	std::cin >> arraySize;
+	int queryType, left, right, x;
+	int numElements, numQueries;
 
-	int arr[arraySize];
-	for (int i = 0; i < arraySize; ++i)
+	std::cin >> numElements >> numQueries;
+
+	int arr[numElements];
+	for (int i = 0; i < numElements; ++i)
 	{
 		std::cin >> arr[i];
 	}
 
-	printArrayInReverseOrder(arraySize, arr);
-
+	for (int i = 0; i < numQueries; ++i)
+	{
+		std::cin >> queryType;
+		if(queryType == 0){
+			std::cin >> left >> right;
+			std::cout << (arr[right-1] ? "ODD" : "EVEN") << std::endl;
+		}
+		else {
+			std::cin >> x;
+			arr[x-1] = arr[x-1] ? 0 : 1;
+		} 
+	}
+	
 	return 0;
 }
